71a2aad4d5735ce82300e3513bc0f66d5eb0eed6
stevdiep-pdx <stevdiep@pdx.edu>
2023-03-12 21:17:46 -0700

    Final touches. Added more functionality with JavaScript.

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-// Get the form
+// Get elements
 let form = document.querySelector('form');
-
-// Get the error message
 let errorMessage = document.querySelector('#error');
+let banner = document.querySelector('#banner');
+let subtitle = document.querySelector('#subtitle');
+let index = 0;
+
+// List of subtitles to cycle through
+let subtitles = [
+  'Software Developer in Training',
+  'CS Student at Portland State University',
+  "Printing 'Hello World'",
+  'Still Clicking?',
+  'Writing Programs...',
+  'Debugging Code...',
+  'Executing Files...',
+];
 
 // When the form is submitted, check its values
 form.addEventListener('submit', (event) => {
@@ -23,4 +35,18 @@ form.addEventListener('submit', (event) => {
   else {
     errorMessage.style.display = 'none';
   }
+});
+
+// When the form is reset, hide the error message
+form.addEventListener('reset', (event) => {
+  errorMessage.style.display = 'none';
+});
+
+// When the banner is clicked, cycle through the list of subtitles
+banner.addEventListener('click', (event) => {
+  subtitle.textContent = subtitles[index];
+  index++;
+
+  // Reset to the beginning of the array once you hit the end
+  if (index == subtitles.length) index = 0;
 });
